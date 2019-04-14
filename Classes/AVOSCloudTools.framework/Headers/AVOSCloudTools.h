3edc87da4b02364b74d9314d76609e9e9f8736fc
appledev201888 <2779461804@qq.com>
2019-04-14 14:47:28 +0800

    提交的描述说明

---
+++ Classes/AVOSCloudTools.framework/Headers/AVOSCloudTools.h
@@ -0,0 +1,14 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVOSCloudTools.
+FOUNDATION_EXPORT double AVOSCloudToolsVersionNumber;
+
+//! Project version string for AVOSCloudTools.
+FOUNDATION_EXPORT const unsigned char AVOSCloudToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVOSCloudTools/PublicHeader.h>
+
+#import <AVOSCloudTools/AVOSCloudToolsManage.h>
+
+
